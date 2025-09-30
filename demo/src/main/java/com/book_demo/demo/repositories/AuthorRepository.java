de810d149ca570d9c143bfccbfb9486edca3a4b9
siputroj <siputroj@gmail.com>
2025-09-29 23:05:18 -0700

    added demo folder to repo

---
+++ demo/src/main/java/com/book_demo/demo/repositories/AuthorRepository.java
@@ -0,0 +1,9 @@
+package com.book_demo.demo.repositories;
+
+import com.book_demo.demo.domain.Author;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthorRepository extends CrudRepository<Author, Long> {
+}
