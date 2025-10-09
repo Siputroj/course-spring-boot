af8835c1b39bc5cc71e5d45cb25fa518ca0f3453
siputroj <siputroj@gmail.com>
2025-10-08 22:51:51 -0700

    create AuthorController.java file

---
+++ demo/src/main/java/com/book_demo/demo/controllers/AuthorController.java
@@ -0,0 +1,30 @@
+package com.book_demo.demo.controllers;
+
+import com.book_demo.demo.domain.dto.AuthorDto;
+import com.book_demo.demo.domain.entities.AuthorEntity;
+import com.book_demo.demo.mappers.Mapper;
+import com.book_demo.demo.services.AuthorService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthorController {
+
+    private final AuthorService authorService;
+    private final Mapper<AuthorEntity, AuthorDto> authorMapper;
+
+    public AuthorController(AuthorService authorService, Mapper<AuthorEntity, AuthorDto> authorMapper) {
+        this.authorService = authorService;
+        this.authorMapper = authorMapper;
+    }
+    @PostMapping(path = "/authors")
+    public ResponseEntity<AuthorDto> createAuthor(@RequestBody AuthorDto author) {
+        AuthorEntity authorEntity = authorMapper.mapFrom(author);
+        AuthorEntity savedAuthorEntity = authorService.createAuthor(authorEntity);
+        return new ResponseEntity<>(authorMapper.mapTo(savedAuthorEntity), HttpStatus.CREATED);
+    }
+
+}
