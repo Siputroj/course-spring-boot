5076e95c5d7870483b12d808f673e8ae303241a1
siputroj <siputroj@gmail.com>
2025-10-08 22:55:23 -0700

    created AuthorService.java file

---
+++ demo/src/main/java/com/book_demo/demo/services/AuthorService.java
@@ -0,0 +1,7 @@
+package com.book_demo.demo.services;
+
+import com.book_demo.demo.domain.entities.AuthorEntity;
+
+public interface AuthorService {
+    AuthorEntity createAuthor(AuthorEntity authorEntity);
+}
